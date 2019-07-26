f46b685414bb839e92e7b8e79ede228d2ccb59f1
Raman <Raman@LAPTOP-JF6HK544>
2019-07-26 13:02:27 +0530

    hi

---
+++ src/main/java/pro1/gittest/App.java
@@ -0,0 +1,13 @@
+package pro1.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
